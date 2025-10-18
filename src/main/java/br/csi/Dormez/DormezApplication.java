1018ce844ed5580cf15f04ef12f7513acfd065fd
Joao Alisson <joaoalissonflores@hotmail.com>
2025-10-18 15:53:44 -0300

    feat: Configurando pom e properties para o Swagger

--- src/main/java/br/csi/Dormez/DormezApplication.java
+++ src/main/java/br/csi/Dormez/DormezApplication.java
@@ -3,10 +3,20 @@ package br.csi.Dormez;
 import br.csi.Dormez.model.CargoEnum;
 import br.csi.Dormez.model.Funcionario;
 import br.csi.Dormez.repository.FuncionarioRepository;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+
+@OpenAPIDefinition(info = @Info(
+		title = "API Exemplo",
+		version = "1.0",
+		description = "Documentação API Exemplo",
+		contact = @Contact(name = "Suporte", email = "suporte@exemplo.com")
+))
 
 @SpringBootApplication
 public class DormezApplication {
