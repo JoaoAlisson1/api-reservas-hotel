bf68d7e7bed6940ead7ceec8f7aa253dcff281cf
Joao Alisson <joaoalissonflores@hotmail.com>
2025-10-25 19:11:11 -0300

    feat(TokenServiceJWT): implementa geração de token JWT e integração no AutenticacaoController

--- src/main/java/br/csi/Dormez/controller/AutenticacaoController.java
+++ src/main/java/br/csi/Dormez/controller/AutenticacaoController.java
@@ -1,10 +1,13 @@
 package br.csi.Dormez.controller;
 
+import br.csi.Dormez.service.TokenServiceJWT;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.token.TokenService;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,9 +18,11 @@ import org.springframework.web.bind.annotation.RestController;
 public class AutenticacaoController {
 
     private final AuthenticationManager Manager;
+    private final TokenServiceJWT tokenServiceJWT;
 
-    public AutenticacaoController(AuthenticationManager Manager) {
+    public AutenticacaoController(AuthenticationManager Manager, TokenServiceJWT tokenServiceJWT) {
         this.Manager = Manager;
+        this.tokenServiceJWT = tokenServiceJWT;
     }
 
     @PostMapping
@@ -25,12 +30,19 @@ public class AutenticacaoController {
         try {
             Authentication autenticado = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
             Authentication at = Manager.authenticate(autenticado);
-            return ResponseEntity.ok().body(at.getPrincipal());
+
+            User user = (User) at.getPrincipal();
+            String token = this.tokenServiceJWT.gerarToken(user);
+
+            return ResponseEntity.ok().body(new DadosTokenJWT(token));
+
         } catch (Exception e) {
             e.printStackTrace();
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+    private record DadosTokenJWT(String token) {}
 
     private record DadosAutenticacao(String login, String senha) {}
 }
---
+++ src/main/java/br/csi/Dormez/service/TokenServiceJWT.java
@@ -0,0 +1,34 @@
+package br.csi.Dormez.service;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+
+@Service
+public class TokenServiceJWT {
+
+    public String gerarToken(User user) {
+        try{
+            Algorithm algorithm = Algorithm.HMAC256("POO2");
+            return JWT.create()
+                    .withIssuer("API Reservas Quartos")
+                    .withSubject(user.getUsername())
+                    .withClaim("ROLE", user.getAuthorities().stream().toList().get(0).toString())
+                    .withExpiresAt(dataExpiracao())
+                    .sign(algorithm);
+        } catch (JWTCreationException e) {
+            throw new RuntimeException("Erro ao gerar token JWT", e);
+        }
+    }
+
+    private Instant dataExpiracao() {
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
+}
