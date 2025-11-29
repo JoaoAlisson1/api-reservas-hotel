5711772b89d7df086d014382821cfd5b63fcbdcd
Joao Alisson <joaoalissonflores@hotmail.com>
2025-11-29 15:53:33 -0300

    mudança da porta da API e modificacoes  na sintaxe em DadosUsuario

--- src/main/java/br/csi/Dormez/DTO/DadosUsuario.java
+++ src/main/java/br/csi/Dormez/DTO/DadosUsuario.java
@@ -2,7 +2,7 @@ package br.csi.Dormez.DTO;
 
 import br.csi.Dormez.model.Usuario;
 
-public record DadosUsuario(Long id, String Login, String permissao) {
+public record DadosUsuario(Long id, String login, String permissao) {
 
     public DadosUsuario(Usuario usuario) {
         this(usuario.getId(), usuario.getLogin(), usuario.getPermissao());
